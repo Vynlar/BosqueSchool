40f5e140e6161da67e832c5350163b29e5152426
Vynlar <lightningzygella@gmail.com>
2014-02-07 10:46:09 -0700

    Moved cal_load into a function

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -1,25 +1,36 @@
-$.getJSON('http://enx3s.com/boscal/cal/?type=json', function(data) {
-	$("#loader").hide();
-	$.each(data[0], function(key, day) {
-		var find = '/';
-		var re = new RegExp(find, 'g');
-		var day_key = key.replace(re, '');
-		var isSchoolDay = false;
-		var label = "No School";
-		$("#cal_list").append('<li id="' + day_key + '" data-role="list-divider"><h2>' + key + "</h2>");
-		$.each(day["en"], function(key2, event) {
-			var split = event.split(" ");
-			if(split[0] == "Day")
+
+var day = "";
+function loadCal()
+{
+	$.getJSON('http://enx3s.com/boscal/cal/?type=json', function(data) {
+		$("#loader").hide();
+		var firstRun = true;
+		$.each(data[0], function(key, day) {
+			var find = '/';
+			var re = new RegExp(find, 'g');
+			var day_key = key.replace(re, '');
+			var isSchoolDay = false;
+			var label = "No School";
+			$("#cal_list").append('<li id="' + day_key + '" data-role="list-divider"><h2>' + key + "</h2>");
+			$.each(day["en"], function(key2, event) {
+				var split = event.split(" ");
+				if(split[0] == "Day")
+				{
+					isSchoolDay = true;
+					label = event;
+				}
+				else
+				{
+					$("#cal_list").append('<li><p>' + event + "</i></p></li>");
+				}
+			});
+			$("#" + day_key).append("<span id='day' class='ui-li-count'>" + label + "</span>");
+			if(firstRun)
 			{
-				isSchoolDay = true;
-				label = event;
-			}
-			else
-			{
-				$("#cal_list").append('<li><p>' + event + "</i></p></li>");
+				day = label;
+				firstRun = false;
 			}
 		});
-		$("#" + day_key).append("<span id='day' class='ui-li-count'>" + label + "</span>");
+		$("#cal_list").listview('refresh');
 	});
-	$("#cal_list").listview('refresh');
-});
+}
